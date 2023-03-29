ab93b9ec4d44165fe848f395bd0d32d023ef2d05
mnaimhaziq <101214356+mnaimhaziq@users.noreply.github.com>
2023-03-29 17:29:43 +0800

    4 user api endpoint created (Backend)
    POST /api/users/login
    POST /api/users/register
    GET /api/users
    PUT /api/user/profile

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 import express from "express";
+import path from "path";
 import dotenv from "dotenv";
 import morgan from "morgan";
+import colors from "colors";
+import connectDB from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
-
+connectDB();
 
 const app = express();
 
@@ -16,8 +20,8 @@ app.get("/", (req, res) => {
   res.send("API is running....");
 });
 
-
+app.use("/api/users", userRoutes);
 
 const PORT = process.env.PORT || 5001;
 
-app.listen(PORT, console.log(`Server running on port ${PORT}`));
+app.listen(PORT, console.log(`Server running on port ${PORT}`.yellow.bold));
